e6ee2b97a2fd02365abd928a2ce2e7f33ae4fbc5
meryam27 <meryamelqemary1@gmail.com>
2025-07-09 01:53:14 +0100

    Création de la navbar coté emploiyé -partie frontent uniquement

--- frontendR/src/App.jsx
+++ frontendR/src/App.jsx
@@ -1,7 +1,17 @@
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
+import Navbar from "./components/employe/Navbar";
+import DashboardEmploye from "./components/employe/DashboardEmploye";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<DashboardEmploye />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
---
+++ frontendR/src/components/employe/DashboardEmploye.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const DashboardEmploye = () => {
+  return <div></div>;
+};
+
+export default DashboardEmploye;
